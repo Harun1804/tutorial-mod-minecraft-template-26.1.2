0ee5e7ee1136a02d67a0a3672cd5f044b307fbc0
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2026-05-11 21:49:22 +0700

    refactor(ModItem): Change order of custom item

--- src/main/java/com/galaxy/item/ModItems.java
+++ src/main/java/com/galaxy/item/ModItems.java
@@ -13,9 +13,6 @@ import net.minecraft.world.item.Item;
 import java.util.function.Function;
 
 public class ModItems {
-    public static final Item FLUORITE = registerItem("fluorite", Item::new);
-    public static final Item RAW_FLUORITE = registerItem("raw_fluorite", Item::new);
-
     private static Item registerItem(String name, Function<Item.Properties, Item> function) {
         return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(TutorialMod26x.MOD_ID, name), function.apply(new Item.Properties()
                 .setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(TutorialMod26x.MOD_ID, name)))));
@@ -29,4 +26,7 @@ public class ModItems {
 //            output.accept(RAW_FLUORITE);
 //        });
     }
+
+    public static final Item FLUORITE = registerItem("fluorite", Item::new);
+    public static final Item RAW_FLUORITE = registerItem("raw_fluorite", Item::new);
 }
